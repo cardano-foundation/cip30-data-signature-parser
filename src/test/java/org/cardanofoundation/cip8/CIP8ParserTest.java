eb3dfbb14e2a6a26cc0f2f76e0ba8fbdfa446f72
Mateusz Czeladka <mateusz.czeladka@cardanofoundation.org>
2022-10-24 22:55:21 +0200

    added more tests

--- src/test/java/org/cardanofoundation/cip8/CIP8ParserTest.java
+++ src/test/java/org/cardanofoundation/cip8/CIP8ParserTest.java
@@ -1,11 +1,9 @@
 package org.cardanofoundation.cip8;
 
-import com.bloxbean.cardano.client.address.util.AddressUtil;
 import com.bloxbean.cardano.client.exception.AddressExcepion;
 import org.junit.jupiter.api.Test;
 
-import java.util.Optional;
-
+import static com.bloxbean.cardano.client.address.util.AddressUtil.bytesToAddress;
 import static org.cardanofoundation.cip8.Format.*;
 import static org.cardanofoundation.cip8.MoreHex.from;
 import static org.cardanofoundation.cip8.ValidationError.NO_PUBLIC_KEY;
@@ -28,7 +26,7 @@ class CIP8ParserTest {
 
         assertArrayEquals(from("e1b83abf370a14870fdfd6ccb35f8b3e62a68e465ed1e096c5a6f5b9d6"), result.getAddress().orElseThrow());
 
-        assertEquals("stake1uxur40ehpg2gwr7l6mxtxhut8e32drjxtmg7p9k95m6mn4s0tdy6k", AddressUtil.bytesToAddress(result.getAddress().orElseThrow()));
+        assertEquals("stake1uxur40ehpg2gwr7l6mxtxhut8e32drjxtmg7p9k95m6mn4s0tdy6k", bytesToAddress(result.getAddress().orElseThrow()));
 
         assertEquals("846a5369676e617475726531582aa201276761646472657373581de1b83abf370a14870fdfd6ccb35f8b3e62a68e465ed1e096c5a6f5b9d640565468697320697320612074657374206d657373616765", result.getCosePayload(HEX));
         assertEquals("5468697320697320612074657374206d657373616765", result.getMessage(HEX));
@@ -60,7 +58,7 @@ class CIP8ParserTest {
     void validSignatureWithoutPublicKey() {
         var sig = "84582aa201276761646472657373581de19090058641fa866e47d656f62be510cb10a90d48b0aafc868f25291ea166686173686564f458ae7b2270726f706f73616c223a2231366436623066393930663563353266393765323338363235623464356362633138333866326439353334313138313664323466643362613234363364666462222c227265717565737465644174223a223734363935373136222c22766f746572223a227374616b6531757867667170767867386167766d6a383665743076326c397a72393370326764667a6332346c797833756a6a6a3873663678763376227d5840ae514d8d246790d728855f69a0ae32b0c5e59f44e00183b20bf110a42d83fa7c209a290b60a65571648220fc36c4efcb9d472e319bf0afdae42fb078085e4206";
 
-        var p = new CIP8Parser(sig, Optional.empty());
+        var p = new CIP8Parser(sig);
         var result = p.parse();
 
         assertFalse(result.isValid());
@@ -68,6 +66,20 @@ class CIP8ParserTest {
 
         assertTrue(result.getAddress().isEmpty(), "strict parser, address not available.");
         assertNull(result.getMessage(), "strict parser, message not available.");
+    }
+
+    @Test
+    void validSignatureWitPublicKeyWithEmptyMessage() throws AddressExcepion {
+        var sig = "84582aa201276761646472657373581de01d813fd4ab9c1e5f7a35da16f75c2e664edfb2a127fc17a4a7ebbfeea166686173686564f44058406ad1822a992684ed10c2802f2c689516254511e92559f19d5288df96f05d002c560d02e0130f73fe2c762170b185d9f9193c3e1efec5f599cb99dfee662d4f0e";
+        var key = "a4010103272006215820c4821499cef96eda9c00cdd0bfbcd2abf7d09436ad424ac7288653a8b4252014";
+
+        var p = new CIP8Parser(sig, key);
+        var result = p.parse();
+
+        assertTrue(result.isValid());
+
+        assertEquals("", result.getMessage(TEXT), "strict parser, message not available.");
+        assertEquals("stake_test1uqwcz0754wwpuhm6xhdpda6u9enyahaj5ynlc9ay5l4mlms4pyqyg", bytesToAddress(result.getAddress().orElseThrow()));
     }
 
 }
